b2dbd35bcdd1d562ad3347e405bb87570ecfa626
Pavan Muddana <mjpavankumar@outlook.com>
2025-04-20 04:00:51 +0530

    Toolbar Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+  
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    RouterModule,
     RouterOutlet,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule, // Import MatToolbarModule
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
